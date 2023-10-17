988721206590871cfec44ea2a55f754491077939
Jonas Earendel <tofsjonas@users.noreply.github.com>
2023-10-17 14:52:48 +0200

    a11y test

--- src/sortable.test.js
+++ src/sortable.test.js
@@ -6,9 +6,11 @@ import fs from 'fs'
 import path from 'path'
 
 const js = fs.readFileSync(path.resolve(__dirname, '../sortable.js'), 'utf8')
+const a11y_js = fs.readFileSync(path.resolve(__dirname, '../sortable.a11y.js'), 'utf8')
 const html = fs
   .readFileSync(path.resolve(__dirname, './sortable.test.html'), 'utf8')
   .replace('<script src="../sortable.js"></script>', `<script>${js}</script>`)
+  .replace('<script src="../sortable.a11y.js"></script>', `<script>${a11y_js}</script>`)
 
 let dom
 let container
@@ -42,7 +44,6 @@ describe('sortable.test.html', () => {
 
   it('sorts a table on click', async () => {
     const table = getAllByRole(container, 'table')[0]
-
     const th = getByRole(table, 'columnheader', { name: /Name/i })
     const first = getAllByRole(table, 'cell')[1].textContent
     fireEvent.click(th)
@@ -159,5 +160,19 @@ describe('sortable.test.html', () => {
     expect(first).toBe('2.49')
     expect(middle).toBe('18.49')
     expect(last).toBe('1.96')
+  })
+
+  it('sorts a table on Enter key', async () => {
+    const table = getAllByRole(container, 'table')[0]
+    const th = getByRole(table, 'columnheader', { name: /Name/i })
+    const first = getAllByRole(table, 'cell')[1].textContent
+    fireEvent.keyDown(th, { key: 'Enter', code: 'Enter' })
+    const middle = getAllByRole(table, 'cell')[1].textContent
+    fireEvent.keyDown(th, { key: 'Enter', code: 'Enter' })
+    const last = getAllByRole(table, 'cell')[1].textContent
+
+    expect(first).toBe('Rick')
+    expect(middle).toBe('Rick')
+    expect(last).toBe('Morty')
   })
 })
