1bcf0cf1e762994cdc8642cf67c986752242e498
Jonas Earendel <tofsjonas@users.noreply.github.com>
2024-11-17 22:59:19 +0900

    demo-folder

--- scripts/prepare-tests.ts
+++ scripts/prepare-tests.ts
@@ -1,9 +1,10 @@
 import fs from 'fs'
 import path from 'path'
 
-const DIST_TYPES = ['bundled', 'standalone', 'esm'] as const
+const DIST_TYPES = ['bundled', 'standalone', 'esm', 'demo'] as const
 
 function createHTML(type: (typeof DIST_TYPES)[number], minified: boolean = false) {
+  if (type === 'demo' && minified) return
   let template = fs.readFileSync('index.html', 'utf8')
 
   // Remove the existing script tags
@@ -12,12 +13,13 @@ function createHTML(type: (typeof DIST_TYPES)[number], minified: boolean = false
     .replace('<script type="module" src="/src/sortable.ts"></script>', '')
     .replace('<script type="module" src="/src/sortable.a11y.ts"></script>', '')
 
-  // Add CSS
-  const cssFiles = minified ? ['sortable.min.css', 'example.min.css'] : ['sortable.css', 'example.css']
+  const css_file_name = `example${minified ? '.min' : ''}.css`
 
-  const cssLinks = cssFiles.map((file) => `<link rel="stylesheet" href="/${file}">`).join('\n')
+  const css_url = type !== 'demo' ? `/${css_file_name}` : `../dist/${css_file_name}`
 
-  template = template.replace('</head>', `${cssLinks}\n</head>`)
+  const css_link = `<link rel="stylesheet" href="${css_url}">`
+
+  template = template.replace('</head>', `${css_link}\n</head>`)
 
   // Generate script tags based on format and minified state
   let scripts = ''
@@ -26,33 +28,45 @@ function createHTML(type: (typeof DIST_TYPES)[number], minified: boolean = false
   switch (type) {
     case 'bundled':
       scripts = `
-        <script src="/sortable${jsExt}"></script>
-        <script src="/sortable.a11y${jsExt}"></script>`
+    <script src="/sortable${jsExt}"></script>
+    <script src="/sortable.a11y${jsExt}"></script>`
+      break
+    case 'demo':
+      scripts = `
+    <script src="../dist/sortable.js"></script>
+    <script src="../dist/sortable.a11y.js"></script>`
       break
     case 'standalone':
       scripts = `
-        <script src="/${type}/sortable${jsExt}"></script>
-        <script src="/${type}/sortable.a11y${jsExt}"></script>`
+    <script src="/${type}/sortable${jsExt}"></script>
+    <script src="/${type}/sortable.a11y${jsExt}"></script>`
       break
     case 'esm':
       scripts = `
-        <script type="module">
-          import './esm/sortable${jsExt}';
-          import './esm/sortable.a11y${jsExt}';
-        </script>`
+    <script type="module">
+      import './esm/sortable${jsExt}';
+      import './esm/sortable.a11y${jsExt}';
+    </script>`
       break
   }
 
   template = template.replace('</body>', `${scripts}\n</body>`)
 
   // Save the file
-  const outputPath = `dist/${type}${minified ? '.min' : ''}.html`
+  const outputPath = type === 'demo' ? 'demo/index.html' : `dist/${type}${minified ? '.min' : ''}.html`
+
+  // if no such directory, create it
+  if (!fs.existsSync(path.dirname(outputPath))) {
+    fs.mkdirSync(path.dirname(outputPath), { recursive: true })
+  }
+
   fs.writeFileSync(outputPath, template)
   console.log(`Created ${outputPath}`)
 }
 
 function cleanup() {
   DIST_TYPES.forEach((type) => {
+    if (type === 'demo') return
     ;['', '.min'].forEach((suffix) => {
       const filePath = `dist/${type}${suffix}.html`
       if (fs.existsSync(filePath)) {
