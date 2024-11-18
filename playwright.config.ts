5d05609d4d4a980fa7bcd645629b1b8570d3d42f
Jonas Earendel <tofsjonas@users.noreply.github.com>
2024-11-18 16:03:51 +0900

    more ci-workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   reporter: process.env.CI ? [['list'], ['html'], ['github']] : [['list'], ['html']],
 
   timeout: 30000,
